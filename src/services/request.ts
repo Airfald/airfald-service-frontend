ceb421789b08e33da34fae51664a6a2856dc7bfa
ouhf <2632734769@qq.com>
2020-03-18 10:30:50 +0800

    feat： 请求统一封装

--- src/services/request.ts
+++ src/services/request.ts
@@ -4,14 +4,15 @@ import qs from "qs";
 // https://www.kancloud.cn/yunye/axios/234845
 const request = axios.create({
   baseURL: '/api',
-  timeout: 5000
+  timeout: 5000,
+  transformResponse: [function (data) {
+    // `transformResponse` 在传递给 then/catch 前，允许修改响应数据
+    return data
+  }]
 });
 
 // 添加请求拦截器
 request.interceptors.request.use(function (config) {
-  console.log('test', process.env);
-  console.log('test', config);
-  console.log('test', request);
   config.headers['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoie1wiYWNjb3VudFwiOlwiaHVhbmdndWFubGlcIixcImNvbnRhY3ROdW1iZXJcIjpcIjE1ODIwMjk2NzE5XCIsXCJjcmVhdGVUaW1lXCI6MTQ4NzcyOTYwNzAwMCxcImVtcGxveWVlTm9cIjpcIlgyMTQxMlwiLFwiaXNEZWxldGVcIjowLFwibmFtZVwiOlwi5YiY5pi-5rSBXCIsXCJub1wiOlwiVVNFMDAwMDAxODA4NFwiLFwib3JnTm9cIjpcIk9SRzAwMDAwMDAwMTIyN1wiLFwic2V4XCI6MCxcInN0YXR1c1wiOjB9IiwiaWQiOiJVU0UwMDAwMDE4MDg0IiwiZXhwIjoxNTg0NTAwNDE2fQ.guWSBFoPiJNIaOiEI_jrkPR-tOZo2KBOyFg7yGDIB8E';
   return config;
 }, function (error) {
@@ -22,7 +23,13 @@ request.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 request.interceptors.response.use(function (response) {
-  return response;
+  // TODO 对权限码进行统一处理
+
+  if (!response.data) {
+    return false;
+  }
+  response.data = JSON.parse(response.data);
+  return response.data;
 }, function (error) {
   // 对响应错误做点什么
   return Promise.reject(error);
