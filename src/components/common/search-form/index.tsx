f1fa316aeef8e83e955e696d515cf72d23677689
ouhf <2632734769@qq.com>
2020-04-10 19:05:02 +0800

    feat(公共搜索组件): 公共搜索组件

--- src/components/common/search-form/index.tsx
+++ src/components/common/search-form/index.tsx
@@ -1,40 +1,208 @@
-import React from 'react'
-import { Button, Input } from 'antd'
+/*
+ * @Author: ouhefu
+ * @Date: 2020-03-20 15:38:57
+ * @LastEditors: ouhefu
+ * @LastEditTime: 2020-04-08 18:35:22
+ * @Description: 封装统一搜索表单，使用数据驱动的方式进行
+ */
+
+import React, { forwardRef } from 'react'
+import { Button, Input, Form, Select, Cascader, DatePicker } from 'antd'
+import { FormComponentProps } from 'antd/lib/form/Form'
+import { commonType } from 'api/types'
 import './index.scss';
 
-const SearchFormCompotent: React.FC<any> = props => {
-  const { formData } = props
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
-  const CustomInput = (props: any) => {
-    const { args } = props
+interface ISearchFormProps extends FormComponentProps {
+  formData: Array<commonType.IFormData>,
+  onQuery: (searchFormData: object) => void
+}
+
+const SearchFormCompotent: React.FC<ISearchFormProps> = props => {
+  const { onQuery } = props
+  const formData = props.formData || []
+  const { getFieldDecorator, getFieldValue, resetFields } = props.form
+
+  // React 传递 ref 给 forwardRef 内函数 (props, ref) => ...，作为其第二个参数。
+  // https://www.lizenghai.com/archives/45474.html
+  const CustomInput = forwardRef((props: any, ref) => {
+    const { filedName, fieldDecoratorArgs } = props
+    const { compontentArgs } = props.compontentConfig
+    const filedNameT: string = filedName
+
     return (
-      <Input
-        {
-          ...args
-        }
-      ></Input>
+        <Input
+          ref={ref}
+          {
+            ...compontentArgs
+          }
+        ></Input>
     )
-  }
+  });
+
+  const CustomSelect = forwardRef((props: any, ref) => {
+    const { compontentArgs, options, keyField, valueField, labelField, width } = props.compontentConfig
+
+    return (
+        <Select
+          ref={ref}
+          {
+            ...compontentArgs
+          }
+          style={{ width: width || '200px' }}
+        >
+          {
+            (options || []).map(optionItem => (
+              <Select.Option key={optionItem[keyField || 'id']} value={optionItem[valueField || 'value']}>
+                {optionItem[labelField || 'label']}
+              </Select.Option>
+            ))
+          }
+        </Select>
+    )
+  });
+
+  const CustomCascader = forwardRef((props: any, ref) => {
+    const { compontentArgs } = props.compontentConfig
+
+    return (
+      <Form.Item>
+        <Cascader
+          ref={ref}
+          {
+            ...compontentArgs
+          }
+        />
+      </Form.Item>
+    )
+  });
 
   const compontentMap = {
-    'input': CustomInput
+    'input': CustomInput,
+    'select': CustomSelect,
+    'cascader': CustomCascader,
+  }
+
+  // const CustomCompontent = compontentMap[item.type]
+
+  {/* // <div key={index}>
+  //   <CustomCompontent
+  //     key={index}
+  //     compontentConfig={item.compontentConfig}
+  //     fieldDecoratorArgs={item.fieldDecoratorArgs}
+  //     filedName={item.filedName}
+  //   ></CustomCompontent>
+  // </div> */}
+
+  // =================================================================================================================
+  // =================================================================================================================
+
+  const handleOnQuery = () => {
+    let searchformData: object = {}
+    formData.forEach(formItem => {
+      const value = getFieldValue(formItem.filedName)
+      if (value) {
+        searchformData[formItem.filedName] = value
+      }
+    })
+
+    onQuery(searchformData)
   }
 
   return (
     <div className='search-form'>
-      {
-        formData.map((item, index) => {
-          const CustomCompontent = compontentMap[item.type]
+       <Form layout="inline">
+        {
+          formData.map((item, index) => {
+            return (
+              <div key={index} className="form-item-wrapper">
+                {
+                  (item.type === 'input') &&
+                  (<Form.Item>
+                    {getFieldDecorator(item.filedName, item.fieldDecoratorArgs)(
+                      <Input
+                        {
+                          ...item.compontentConfig.compontentArgs
+                        }
+                      ></Input>
+                    )}
+                  </Form.Item>)
+                }
 
-          return (
-            <CustomCompontent
-              args={item.args}
-            ></CustomCompontent>
-          )
-        })
-      }
+                {
+                  (item.type === 'cascader') &&
+                  (<Form.Item>
+                    {getFieldDecorator(item.filedName, item.fieldDecoratorArgs)(
+                      <Cascader
+                        {
+                          ...item.compontentConfig.compontentArgs
+                        }
+                      />
+                    )}
+                  </Form.Item>)
+                }
+
+                {
+                  (item.type === 'RangePicker') &&
+                  (<Form.Item>
+                    {getFieldDecorator(item.filedName, item.fieldDecoratorArgs)(
+                      <RangePicker
+                        {
+                          ...item.compontentConfig.compontentArgs
+                        }
+                      />
+                    )}
+                  </Form.Item>)
+                }
+
+                {
+                  (item.type === 'DatePicker') &&
+                  (<Form.Item>
+                    {getFieldDecorator(item.filedName, item.fieldDecoratorArgs)(
+                      <DatePicker
+                        {
+                          ...item.compontentConfig.compontentArgs
+                        }
+                      />
+                    )}
+                  </Form.Item>)
+                }
+
+                {(item.type === 'select') && (<Form.Item>
+                  {getFieldDecorator(item.filedName, item.fieldDecoratorArgs)(
+                    <Select
+                      {
+                        ...item.compontentConfig.compontentArgs
+                      }
+                      style={{ width: item.compontentConfig.width || '200px' }}
+                    >
+                      {
+                        (item.compontentConfig.options || []).map(optionItem => (
+                          <Select.Option key={optionItem[item.compontentConfig.keyField || 'id']} value={optionItem[item.compontentConfig.valueField || 'value']}>
+                            {optionItem[item.compontentConfig.labelField || 'label']}
+                          </Select.Option>
+                        ))
+                      }
+                    </Select>
+                  )}
+                </Form.Item>)}
+            </div>
+            )
+          })
+        }
+          <Form.Item>
+            <Button
+              type="primary"
+              onClick={() => handleOnQuery()}
+            >
+              查询
+            </Button>
+            <Button onClick={() => resetFields()}>重置</Button>
+          </Form.Item>
+        </Form>
     </div>
   );
 };
 
-export default SearchFormCompotent
+export default Form.create<ISearchFormProps>({ name: 'SearchFormCompotent' })(SearchFormCompotent);
--- src/views/frame/search-list/index.tsx
+++ src/views/frame/search-list/index.tsx
@@ -3,24 +3,132 @@ import { Button } from 'antd'
 import { connect } from 'react-redux';
 import SearchFormCompotent from 'components/common/search-form'
 import './index.scss';
+import { commonType } from 'api/types'
 
 const SearchListCompotent: React.FC = props => {
-  const formData = [
+  const formData: Array<commonType.IFormData> = [
     {
-      type: 'input'
+      type: 'input',
+      filedName: 'name',
+      compontentConfig: {
+        compontentArgs: {
+          placeholder: '文章名称'
+        }
+      }
     },
     {
-      type: 'input'
-    }
+      type: 'select',
+      filedName: 'type',
+      compontentConfig: {
+        compontentArgs: {
+          placeholder: '类型',
+          mode: 'multiple'
+        },
+        options: [
+          {
+            id: 1,
+            label: '前端',
+            value: 1
+          },
+          {
+            id: 2,
+            label: '后端',
+            value: 2
+          },
+          {
+            id: 3,
+            label: '开发语言',
+            value: 3
+          },
+          {
+            id: 4,
+            label: '框架',
+            value: 4
+          }
+        ]
+      }
+    },
+    {
+      type: 'select',
+      filedName: 'time',
+      compontentConfig: {
+        compontentArgs: {
+          placeholder: '预计阅读时间',
+        },
+        options: [
+          {
+            id: 1,
+            label: '三分钟内',
+            value: 1
+          },
+          {
+            id: 2,
+            label: '五分钟内',
+            value: 2
+          },
+          {
+            id: 3,
+            label: '大于五分钟',
+            value: 3
+          }
+        ]
+      }
+    },
+    {
+      type: 'cascader',
+      filedName: 'region',
+      compontentConfig: {
+        compontentArgs: {
+          placeholder: '发布地区',
+          options: [
+            {
+              value: 'zhejiang',
+              label: 'Zhejiang',
+              children: [
+                {
+                  value: 'hangzhou',
+                  label: 'Hangzhou',
+                  children: [
+                    {
+                      value: 'xihu',
+                      label: 'West Lake',
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+    {
+      type: 'RangePicker',
+      filedName: 'range',
+      compontentConfig: {
+        compontentArgs: {
+          // placeholder: ''
+        }
+      }
+    },
+    {
+      type: 'DatePicker',
+      filedName: 'date',
+      compontentConfig: {
+        compontentArgs: {
+          placeholder: '请选择'
+        }
+      }
+    },
   ]
 
   return (
     <div className='search-list'>
-      search-List
       <SearchFormCompotent
         formData={formData}
+        onQuery={(data) => {
+          console.log('data ', data)
+        }}
       ></SearchFormCompotent>
-      {/* search-form */}
       {/* table */}
       {/* pagniation */}
     </div>
