321405df8cd8c0931f59a63e20c0669bc9dc0da3
ouhf <2632734769@qq.com>
2020-04-22 11:58:06 +0800

    feat(文章编辑): 添加表单

--- src/views/article/edit/index.tsx
+++ src/views/article/edit/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Button } from 'antd'
-import { Editor } from '@tinymce/tinymce-react';
+import { Button, Form, Input } from 'antd'
 import { Link, useHistory } from 'react-router-dom'
+import { FormComponentProps } from 'antd/lib/form/Form'
+import { Editor } from '@tinymce/tinymce-react'
+
 import { connect } from 'react-redux';
 import { EditorConfig } from '../constant'
 import './index.scss';
 
+interface IArticleEditProps extends FormComponentProps {
+}
+
 // https://www.tiny.cloud/docs/integrations/react/#tinymcereactintegrationquickstartguide
-const ArticleEditCompotent: React.FC = props => {
-  let history = useHistory();
+const ArticleEditCompotent: React.FC<IArticleEditProps> = props => {
+  const history = useHistory();
+  const {
+    form: { getFieldDecorator, getFieldsValue }
+  } = props
 
   const handleEditorChange = (content, editor) => {
     console.log('Content was updated:', content);
@@ -16,11 +24,43 @@ const ArticleEditCompotent: React.FC = props => {
 
   return (
     <div className='article-edit'>
-      <Editor
-         initialValue={EditorConfig.defaultInitialValue}
-         init={EditorConfig}
-         onEditorChange={handleEditorChange}
-       />
+      <Form
+        labelCol={{ span: 2 }}
+        wrapperCol={{ span: 10 }}
+        labelAlign="left">
+        <Form.Item label="名称">
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: '请输入名称' }],
+          })(
+            <Input placeholder="请输入名称" allowClear />
+          )}
+        </Form.Item>
+        <Form.Item label="类型">
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: '请输入类型' }],
+          })(
+            <Input placeholder="请输入类型" allowClear />
+          )}
+        </Form.Item>
+        <Form.Item label="标签">
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: '请选择标签' }],
+          })(
+            <Input placeholder="请输入名称" allowClear />
+          )}
+        </Form.Item>
+        <Form.Item label="编辑文章">
+          <Editor
+            initialValue={EditorConfig.defaultInitialValue}
+            init={EditorConfig}
+            onEditorChange={handleEditorChange}
+          />
+        </Form.Item>
+      </Form>
+      <div className="article-edit__operation">
+        <Button type="default">取消</Button>
+        <Button type="primary" className="ml15">确定</Button>
+      </div>
     </div>
   );
 };
@@ -36,4 +76,6 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(ArticleEditCompotent)
+)(Form.create<IArticleEditProps>({ name: 'GrouponEditor' })(
+  ArticleEditCompotent
+))
